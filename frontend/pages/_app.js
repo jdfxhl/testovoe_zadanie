68e11cbca675488906996e03f60c3ba596b3a854
Krasnova_lab6 <160151952+jdfxhl@users.noreply.github.com>
2026-06-21 16:49:22 +0500

    feat: Next.js _app.js with ToastContainer

---
+++ frontend/pages/_app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/globals.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  );
+}
+
+export default MyApp;
